122db2380bc231e627206ef5be08a05ccb0f9811
kahvimorso <ottoheiskanen@gmail.com>
2023-02-18 20:37:39 +0200

    player class added

---
+++ Player.js
@@ -0,0 +1,66 @@
+export default class Player {
+    constructor(id, name, room, balance, hand, ready, display, position) {
+        this.id = id // set this to socket id
+        this.name = name
+        this.room = room
+        this.balance = balance
+        this.hand = hand
+        this.ready = ready
+        this.display = display
+        this.position = position
+
+        this.x = 0
+        this.y = 0
+
+        switch(this.position) {
+            case 1:
+                this.x = 204
+                this.y = 20; break;
+            case 2:
+                this.x = 20
+                this.y = 160; break;
+            case 3: 
+                this.x = 375
+                this.y = 160; break;
+            case 4: 
+                this.x = 204
+                this.y = 268; break;
+        }
+
+        this.ctx = document.getElementById("game").getContext("2d");
+
+        this.cardW = 50
+        this.cardH = 75
+
+        this.switch = [] 
+    }
+
+    move() {
+        
+    }
+
+    switchCards(newCards = []) {
+
+    }
+
+    update() {
+        
+    }
+
+    draw() {
+        this.ctx.font  = "18px serif"
+        this.ctx.fillStyle = "black"
+        this.ctx.fillText(this.name, this.x, this.y-4)
+
+        
+        for (let i = 0; i < this.hand.length; i++) {
+            this.ctx.fillStyle = "white"
+            this.ctx.fillRect(this.x + i * this.cardW, this.y, this.cardW-5, this.cardH)
+            this.ctx.fillStyle = "black"
+            this.ctx.fillText(this.hand[i], this.x + (i * this.cardW), this.y+32)
+        }
+    }
+
+}
+
+//module.exports = Player
--- public/game.js
+++ public/game.js
@@ -1,1 +1,59 @@
-console.log("works")
+import Player from "../Player.js"
+game.width = 640
+game.height = 480
+const ctx = game.getContext("2d")
+let secondsPassed
+let oldTimeStamp
+let fps
+
+let gameObjects = []
+
+// Search for id that was returned from the server side and get right card values to checkboxs
+function getCardInfo(id) {
+    for (let i = 0; i < gameObjects.length; i++) {
+        if (gameObjects[i].id === id) {
+            cl1.innerText = gameObjects[i].hand[0]
+            cl2.innerText = gameObjects[i].hand[1]
+            cl3.innerText = gameObjects[i].hand[2]
+            cl4.innerText = gameObjects[i].hand[3]
+            cl5.innerText = gameObjects[i].hand[4]
+        }
+    }
+}
+
+function init() {
+    gameObjects.push(new Player(1, "otto", "paska", 500, ['a5', 'a4', 'a3', 'a2', 'a1'], false, false, 1))
+    gameObjects.push(new Player(2, "ilari", "paska", 500, ['b5', 'b4', 'b3', 'b2', 'b1'], false, false, 2))
+    gameObjects.push(new Player(3, "ville", "paska", 500, ['c5', 'c4', 'c3', 'c2', 'c1'], false, false, 3))
+    gameObjects.push(new Player(4, "nakkeri", "paska", 500, ['d5', 'd4', 'd3', 'd2', 'd1'], false, false, 4))
+    getCardInfo(1)
+    window.requestAnimationFrame(gameLoop)
+}
+
+function gameLoop(timeStamp) {
+    // Calculate the number of seconds passed since the last frame
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000
+    oldTimeStamp = timeStamp
+
+    // Calculate fps
+    fps = Math.round(1 / secondsPassed)
+
+    console.log(fps)
+
+    draw()
+    window.requestAnimationFrame(gameLoop)
+}
+
+function clear() {
+    ctx.clearRect(0,0, game.width, game.height)
+}
+
+function draw() {
+    clear()
+    for (let i = 0; i < gameObjects.length; i++) {
+        gameObjects[i].update()
+        gameObjects[i].draw()
+    }
+}
+
+init()
